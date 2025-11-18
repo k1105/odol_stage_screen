ef5aea055776df742926e9386ed032c627dfb126
k1105 <kntymgs1105@gmail.com>
2025-11-18 23:34:17 +0900

    テキストのimport方法を修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,18 +30,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${delaGothicOne.variable}`}
       >
